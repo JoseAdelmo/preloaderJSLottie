d1f062f36c83c019f05d97eb21a1ffd951907004
José Adelmo <99682808+JoseAdelmo@users.noreply.github.com>
2024-08-21 16:27:40 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,6 +1,6 @@
 const overlay = document.createElement('div');
 overlay.id = 'loader';
-overlay.style.cssText = 
+overlay.style.cssText = `
     display: flex;
     justify-content: center;
     align-items: center;
@@ -11,26 +11,25 @@ overlay.style.cssText =
     height: 100%;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
-;
+`;
 
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
-lottieContainer.style.cssText = 
+lottieContainer.style.cssText = `
     max-width: 100%;
     max-height: 100%;
-    display: none;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-;
-lottieContainer.style.display = 'none';
+`;
+
+overlay.appendChild(lottieContainer);
 
 document.body.appendChild(overlay);
-document.body.appendChild(lottieContainer);
 
-function hideOverlay() {
-    overlay.style.display = 'none';
+function showLoader() {
+    overlay.style.display = 'flex'; // Exibe o overlay e o Lottie
     lottieContainer.style.display = 'block';
     lottie.loadAnimation({
         container: lottieContainer,
@@ -41,8 +40,10 @@ function hideOverlay() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    hideOverlay();
-});
+function hideLoader() {
+    overlay.style.display = 'none';
+}
 
-window.addEventListener('load', hideOverlay);
+showLoader();
+
+window.addEventListener('load', hideLoader);
